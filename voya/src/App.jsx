b7855a38bbb663db767a0104e1c821d3755d509a
Laura Vesamäki <laura.vesamaki@live.fi>
2025-05-15 13:49:50 +0300

    voya setup

---
+++ voya/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="header">
+        <h1>Voya</h1>
+      </div>
+      <div className="container">
+        <h2>Chat</h2>
+        <div className="chat-container">
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
