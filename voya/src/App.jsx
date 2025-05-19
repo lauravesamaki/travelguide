8891452696224eb8271f51261234fe74ea1826ea
Laura Vesamäki <laura.vesamaki@live.fi>
2025-05-19 15:01:26 +0300

    added ui for chatbot

--- voya/src/App.jsx
+++ voya/src/App.jsx
@@ -1,6 +1,32 @@
 import './App.css'
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  const sendMessage = async () => {
+    const userMessage = {
+      role: 'user',
+      content: input
+    };
+    setMessages([...messages, userMessage]);
+    setInput('');
+
+    try {
+      const res = await axios.post('http://localhost:8000/chat', {
+        message: input});
+      const botMessage = {
+        role: 'assistant',
+        content: res.data.response
+      };
+      setMessages(prev => [...prev, botMessage]);
+    }
+    catch (error) {
+      console.error('Error sending message:', error);
+    }
+  };
 
   return (
     <>
@@ -8,9 +34,23 @@ function App() {
         <h1>Voya</h1>
       </div>
       <div className="container">
-        <h2>Chat</h2>
         <div className="chat-container">
-
+          <div className="messages">
+            {messages.map((message, index) => (
+              <div key={index} class={`${message.role}` === 'user' ? 'chat chat-sender' : 'chat chat-receiver'}>
+                <div class='chat-bubble'>{message.content}</div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="input-container">
+          <input
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            onKeyDown={(e) => e.key === 'Enter' && sendMessage(e)}
+          />
+          <button onClick={sendMessage}>Send</button>
         </div>
       </div>
     </>
